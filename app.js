189c23ce3c3c2f3a5e42bb4eef4670353e37302e
loupgarou81 <leloupgarou81@gmail.com>
2016-07-29 00:07:48 +0100

    Updated app with redis and tests

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ var bodyParser = require('body-parser');
 var urlencode = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
 
+// use refis npm
 var redis = require('redis');
 var client = redis.createClient();
 
-client.hset('cities', 'Lotopia', 'description');
-client.hset('cities', 'Caspiana', 'description');
-client.hset('cities', 'Indigo', 'description');
+client.select((process.env.NODE_ENV || 'development').length);
+// create starting data
+// client.hset('cities', 'Lotopia', 'description');
+// client.hset('cities', 'Caspiana', 'description');
+// client.hset('cities', 'Indigo', 'description');
 
 app.get('/cities', function(req,res){
+  // get cities
   client.hkeys('cities', function(error, names){
     res.json(names);
   });
 });
 
 app.post('/cities', urlencode, function(req, res){
+  // retrieve data from the form
   var newCity = req.body;
+  // create data in db
   client.hset('cities', newCity.name, newCity.description, function(error){
     if(error) throw error;
     res.status(201).json(newCity.name);
