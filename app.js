696843feda95cd0054904aef0eb7781d05e53c52
loupgarou81 <leloupgarou81@gmail.com>
2016-07-28 23:48:30 +0100

    Added redis

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ var bodyParser = require('body-parser');
 var urlencode = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
 
-var cities = {'Lotopia': 'description', 'Caspiana': 'description', 'Indigo': 'description'};
+var redis = require('redis');
+var client = redis.createClient();
+
+client.hset('cities', 'Lotopia', 'description');
+client.hset('cities', 'Caspiana', 'description');
+client.hset('cities', 'Indigo', 'description');
 
 app.get('/cities', function(req,res){
-  res.json(Object.keys(cities));
+  client.hkeys('cities', function(error, names){
+    res.json(names);
+  });
 });
 
 app.post('/cities', urlencode, function(req, res){
   var newCity = req.body;
-  cities[newCity.name] = newCity.description;
-  res.status(201).json(newCity.name);
+  client.hset('cities', newCity.name, newCity.description, function(error){
+    if(error) throw error;
+    res.status(201).json(newCity.name);
+  });
 });
 
 module.exports = app;
